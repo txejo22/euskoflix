81f5b2a5965251ac773b8257b71bfa8222ccc510
Sergio <Sergio@192.168.0.15>
2019-04-09 17:17:52 +0200

    test

---
+++ src/euskoflix/test/prueba.java
@@ -0,0 +1,37 @@
+package euskoflix.test;
+/*
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import euskoflix.modelo.HMIntegerDouble;
+import euskoflix.modelo.HMStringDouble;
+import euskoflix.modelo.MatrizValoraciones;
+import euskoflix.modelo.ModeloPersonas;
+import euskoflix.modelo.ModeloProducto;
+import euskoflix.principal.Euskoflix;
+
+	public class prueba {
+		Euskoflix e=Euskoflix.getEuskoflix();
+		e.cargarEstrucutras();	
+		HMIntegerDouble matrizValoraciones=MatrizValoraciones.getValoracionesUsuario().getMatriz();
+		HMStringDouble modeloProducto=ModeloProducto.getModeloProducto().crearModeloProducto();
+		HMStringDouble modeloPersona=ModeloPersonas.getModeloPersonas().crearModeloPersona(matrizValoraciones, modeloProducto, 3.5);
+		HMIntegerDouble matrizSimilitud=ModeloPersonas.getModeloPersonas().crearSimilitud(modeloProducto, modeloPersona);
+	
+		@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}*/
